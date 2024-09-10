cce73f49db96e9c9db88fff53f93393978b7151f
1id0r <lidornissim44@gmail.com>
2024-09-10 15:12:02 +0300

    add ball limit to 400

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function onBallClick() {
   const elBall = document.querySelector('.ball')
   var currWidth = elBall.offsetWidth
   var currHeight = elBall.offsetHeight
+  if (currWidth > 400) {
+    currWidth = 50
+    currHeight = 50
+  }
   var newHeight = currHeight + 50
   var newWidth = currWidth + 50
   elBall.style.height = newHeight + 'px'
